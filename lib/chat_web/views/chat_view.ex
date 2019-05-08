c4209f3c78cbb726d0b252048c258bdbf842f719
Alan Vardy <alanvardy@gmail.com>
2019-05-07 20:27:43 -0600

    time

--- lib/chat_web/views/chat_view.ex
+++ lib/chat_web/views/chat_view.ex
@@ -1,3 +1,12 @@
 defmodule ChatWeb.ChatView do
   use ChatWeb, :view
+
+  def nice_date(datetime) do
+    {:ok, result} =
+      datetime
+      |> Timex.Timezone.convert("America/Edmonton")
+      |> Timex.format("{h24}:{m}:{s}")
+
+    result
+  end
 end
--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Chat.MixProject do
   def application do
     [
       mod: {Chat.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :timex]
     ]
   end
 
@@ -43,7 +43,8 @@ defmodule Chat.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"}
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:timex, "~> 3.0"}
     ]
   end
 
